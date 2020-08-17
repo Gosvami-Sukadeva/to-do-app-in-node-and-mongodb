0672cd6b8f22bf6c832219fe39d405848060afd0
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-08-17 14:54:32 +0200

    add function addNewTodo

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const mongo = require('mongodb');
 const client = new mongo.MongoClient('mongodb://localhost:27017', {useNewUrlParser: true});
 
+function addNewTodo(todosCollection,title){
+    todosCollection.insertOne({
+        title,
+        done: false,
+    },err => {
+        if (err){
+            console.log('Błąd podczas dodawania!', err)
+        }else {
+            console.log('Zadanie dodane.')
+        }
+        client.close();
+    })
+    
+    
+}
+
+
 function doTheToDo(todosCollection) {
 const [command, ...args] = process.argv.splice(2);
-console.log(command, args)
-    client.close();
+switch(command){
+    case 'add':
+        addNewTodo(todosCollection, args[0]);
+        break;
+}
+    // client.close();
 }
 
 client.connect(err => {
